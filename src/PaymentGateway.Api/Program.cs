3a22db0c20efdd4ce66433ba9e8a5ed00ef317e1
Bruno Fernando Corrêa de Abreu <bruno.feabreu@gmail.com>
2025-07-30 22:07:39 -0300

    tests - Added Controller Tests

--- src/PaymentGateway.Api/Program.cs
+++ src/PaymentGateway.Api/Program.cs
@@ -1,15 +1,31 @@
+using System.Text.Json.Serialization;
+
+using PaymentGateway.Api.Interfaces;
 using PaymentGateway.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<PaymentsRepository>();
+builder.Services.AddTransient<IPaymentService, PaymentsService>();
+builder.Services.AddTransient<IBankHttpClient, AcquiringBankHttpClient>();
+builder.Services.AddHttpClient<IBankHttpClient, AcquiringBankHttpClient>(c =>
+{
+    c.BaseAddress = new Uri(builder.Configuration.GetValue<string>("AcquiringBankClientUrl") ?? string.Empty);
+    c.DefaultRequestHeaders.Add("Accept", "application/json");
+});
 
 var app = builder.Build();
 
