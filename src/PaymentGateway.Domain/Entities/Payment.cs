53dd06fd6fe6d04f3fc7126d26eb0f06217bcb83
Bruno Fernando Corrêa de Abreu <bruno.feabreu@gmail.com>
2025-07-31 22:27:19 -0300

    chore - Changed prop AuthCode on Payment entity to private set

--- src/PaymentGateway.Domain/Entities/Payment.cs
+++ src/PaymentGateway.Domain/Entities/Payment.cs
@@ -25,7 +25,7 @@ public class Payment
     public int Amount { get; private set; }
     public string CVV { get; private set; }
     public PaymentStatus Status { get; private set; }
-    public string AuthorizationCode { get; set; }
+    public string AuthorizationCode { get; private set; }
     
     public void SetAuthorized(string authorizationCode)
     {
